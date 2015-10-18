0fb821aebcaf7c7fb07e3e017dc5f4d9550443d4
Frank Shen <zhshen@gmail.com>
2015-10-18 16:37:25 +0800

    依測試程式新增ProductInfo, IDataDao, GroupService

---
+++ tdd-day1-homework/GroupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tdd_day1_homework
+{
+    public class GroupService
+    {
+        public int[] GetPagingData(int pagingSize, string sumField)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tdd-day1-homework/ProductInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tdd_day1_homework
+{
+    public class ProductInfo
+    {
+    }
+}
